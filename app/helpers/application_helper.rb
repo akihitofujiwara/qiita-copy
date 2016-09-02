986bfa95228235e3bb93d5d2ddfb669ee1f11f8c
Akihito Fujiwara <akitoha1207@gmail.com>
2016-09-02 09:18:03 +0900

    Add styles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def css_namespace
+    "#{controller.controller_name} #{controller.action_name}"
+  end
+
+  def render_unless_empty(items)
+    items.length > 0 ?
+      render(items)
+    : "<div class=\"no-items\">No Items</div>".html_safe
+  end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,10 +1,11 @@
 module ItemsHelper
-  def stock_link_of(item)
+  def stock_link_of(item, options = {})
+    btn_class = "btn btn-#{options[:btn_type] || "primary"}"
     if user_signed_in?
       if current_user.stocks? item
-        link_to "Unstock", item_stock_path(item), method: :delete, class: "btn btn-danger"
+        link_to "Unstock", item_stock_path(item), method: :delete, class: "btn btn-#{options[:btn_type] || "danger"}"
       else
-        link_to "Stock", item_stock_path(item), method: :post, class: "btn btn-primary"
+        link_to "Stock", item_stock_path(item), method: :post, class: "btn btn-#{options[:btn_type] || "primary"}"
       end
     end
   end
