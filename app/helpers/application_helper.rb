2d79794ce247563a85a09ea5068a1c2f227bfe42
Akihito Fujiwara <akitoha1207@gmail.com>
2016-09-10 12:06:02 +0900

    Improve home views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,4 @@ module ApplicationHelper
       user_signed_in? ? "sined-in" : "not-signed-in"
     ].join " "
   end
-
-  def render_unless_empty(items)
-    items.length > 0 ?
-      render(items)
-    : "<div class=\"no-items\">No Items</div>".html_safe
-  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,17 @@ module HomeHelper
   def active_in(path)
     "active" if current_page? path
   end
+
+  def render_unless_empty(items)
+    items.length > 0 ?
+      render(items)
+    : (<<"no-items"
+<p class="no-items">
+  投稿はまだありません。
+  <br>
+  <a href="/drafts/new">Qiita全体の投稿</a>を見て、あなたの持っているtips、コードを投稿したり、タグやユーザーをフォローしてみましょう!
+</p>
+no-items
+      ).html_safe
+  end
 end
