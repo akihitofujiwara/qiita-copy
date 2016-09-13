402adc931edba5d9b85e25ecd64e882a74e4b919
Akihito Fujiwara <akitoha1207@gmail.com>
2016-09-13 23:57:04 +0900

    Modify migration 'add published_at to items'.

--- db/migrate/20160910042515_add_published_at_to_items.rb
+++ db/migrate/20160910042515_add_published_at_to_items.rb
@@ -1,5 +1,10 @@
 class AddPublishedAtToItems < ActiveRecord::Migration
-  def change
+  def up
     add_column :items, :published_at, :datetime
+    Item.update_all 'published_at=created_at'
+  end
+
+  def down
+    remove_column :items, :published_at, :datetime
   end
 end
