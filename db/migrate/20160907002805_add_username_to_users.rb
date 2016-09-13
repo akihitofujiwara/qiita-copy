805647fe098dd42c46c4aa92ab6e63814a93c7e7
Akihito Fujiwara <akitoha1207@gmail.com>
2016-09-14 01:29:14 +0900

    Modify migration 'add username to users'.

--- db/migrate/20160907002805_add_username_to_users.rb
+++ db/migrate/20160907002805_add_username_to_users.rb
@@ -1,6 +1,11 @@
 class AddUsernameToUsers < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :username, :string
+    User.all.each { |user| user.update username: (user.email.match(/([^@]+)@/)[1]) }
     add_index :users, :username, unique: true
+  end
+
+  def down
+    remove_column :users, :username
   end
 end
