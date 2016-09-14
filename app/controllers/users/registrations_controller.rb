3163da76a3c033cedef90d774c29c948d9c0da2c
Akihito Fujiwara <akitoha1207@gmail.com>
2016-09-14 10:57:54 +0900

    Modify user creation by twitter.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
     redirect_to :back, notice: "更新しました。"
   end
 
+  def create
+    if user = (User.find_by_email user_params[:email])
+      user.update user_params.merge({
+        provider: (attrs = session["user_attrs"])["provider"],
+        uid: attrs["uid"],
+        icon_url: attrs["info"]["image"]
+      })
+      sign_in user
+      redirect_to root_path
+    else
+      super
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email)
